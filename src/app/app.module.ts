621ac4c32b63ac63f1b5eea435f79710aff7014d
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2020-06-15 14:29:59 +0100

    added new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  HttpModule } from '@angular/http';
 import { AppComponent} from './app.component';
-import { EntryListComponent } from './enteries';
+import { EntryListComponent, EntryComponent } from './enteries';
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
     declarations: [
         AppComponent,
+        EntryComponent,
         EntryListComponent
     ],
     bootstrap: [AppComponent]
